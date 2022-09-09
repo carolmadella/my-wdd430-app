05184f4c4ca0c6195893386b4d5f612a1c9c93dc
Aaron Kay <kaychoro@gmail.com>
2022-09-08 22:48:00 -0600

    Ready to go

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-        try {
-            new prove7.ProveLinkedLists().run();
-        }
-        catch (Exception ignore) {
-        }
+﻿/*
+ * CSE212 
+ * (c) BYU-Idaho
+ * 01-Teach
+ * 
+ * It is a violation of BYU-Idaho Honor Code to post or share this code with others or 
+ * to post it online.  Storage into a personal and private repository (e.g. private
+ * GitHub repository, unshared Google Drive folder) is acceptable.
+ */
+
+using teach_01;
+
+Console.WriteLine("\n======================\nDivisors\n======================");
+Divisors.Run();
+
+Console.WriteLine("\n======================\nArray Selector\n======================");
+ArraySelector.Run();
+
+// Uncomment and run as you get to the solution part
+// Console.WriteLine("\n======================\nDivisors\n======================");
+// DivisorsSolution.Run();
+//
+// Console.WriteLine("\n======================\nArray Selector\n======================");
+// ArraySelectorSolution.Run();
