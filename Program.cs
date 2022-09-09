3d560f269aa410c97e128ce4c2afcb269697e390
Aaron Kay <kaychoro@gmail.com>
2022-09-09 09:41:23 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,8 +17,8 @@ Console.WriteLine("\n======================\nArray Selector\n===================
 ArraySelector.Run();
 
 // Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nDivisors\n======================");
+// Console.WriteLine("\n======================\nDivisors Solution\n======================");
 // DivisorsSolution.Run();
 //
-// Console.WriteLine("\n======================\nArray Selector\n======================");
+// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
 // ArraySelectorSolution.Run();
