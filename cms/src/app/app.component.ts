d2cd7b64a759952e83e077658bb2a26fd4d11040
Carolina Madella <carolina.madella@gmail.com>
2025-09-28 21:27:43 -0500

    file structure

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cms-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  title = 'cms';
 }
