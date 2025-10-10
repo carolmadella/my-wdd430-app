f485d62bb2f0a84ec0860741b012b7a9b7469d42
Carolina Madella <carolina.madella@gmail.com>
2025-10-10 11:47:31 -0500

    week 2

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -9,12 +9,5 @@ import { Contact } from '../../models/contact.model';
 })
 export class ContactDetailComponent {
 
-  contact = new Contact(
-  '1',
-  'R. Kent Jackson',
-  'jacksonk@byui.edu',
-  '208-496-3771',
-  '../../assets/images/jacksonk.jpg',
-  null
-);
+  contact!: Contact;
 }
